07ab06b01873606a0a7f8bb32615c91c5599c2e1
whoato <whoato@gmail.com>
2017-03-29 19:58:17 +0900

    Create 170329_Switch3.c

---
+++ 170329_Switch3.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	if (조건)
+	{
+		조건이 맞으면 실행할 내용.
+	}
+	*/
+	/*
+
+	// int iNum = 1;	// true;
+	int iNum = 0;		// false;
+
+	if (iNum)
+	{
+		printf("이 조건은 [참]입니다.\n");
+	}
+
+	*/
+	/*
+	if (0)
+	{
+		printf("조건 A\n");
+	}
+	else if(1)
+	{
+		printf("조건 B\n");
+	}
+	else if(1)
+	{
+		printf("조건 C\n");
+	}
+	else
+	{
+		printf("모든 조건이 False \n");
+	}
+	*/
+	/*
+	int n = 65;
+
+	int iNum = 1;
+
+	switch (n)
+	{//상수 && 정수형
+		// case iNum -> error C2051
+		// case 3.14 -> error C2052
+		case 1:
+			printf("숫자 1 입니다.\n");
+			break;
+
+		case 2:
+			printf("숫자 2 입니다.\n");
+			break;
+
+		case 'A':
+			printf("A 입니다.\n");			
+			break;
+
+		default:
+			printf("입력값에 대한 정보가 없습니다.\n");
+			break;
+	}
+
+	// switch문은 if문보다 빠르다.  권장사항. 필수사항 아님.
+	// switch 조건 = 상수형 && 정수형.
+	// 문자열과 실수형은 사용할 수 없다.
+	// 조건에 연산자를 사용할 수 없다.
+	// 연산식을 사용할 수 없다.
+
+
+	// 삼항연산자와 같이 쓸 수 있지 않을까?
+
+	*/
+
+	// 예습 반복 문
+	// while() 문
+}
